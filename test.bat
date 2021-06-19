264149605e77656876c0a8df9739a347dd0b5d48
veganaize <veganaize@users.noreply.github.com>
2021-06-18 18:46:39 -0700

    Windows NT calls

--- test.bat
+++ test.bat
@@ -3,6 +3,7 @@ SETLOCAL
 set next=0
 
 
+REM -- TEST: NO ARGS --
 type     >type_cmd.1 2>NUL
 type.exe >type_exe.1 2>NUL
 type     2>type_cmd.2 >NUL
@@ -12,6 +13,7 @@ GOTO ERRORCHECK
 :AFTERTEST1
 
 
+REM -- TEST: HELP SWITCH --
 type/?     >type_cmd.1 2>NUL
 type.exe/? >type_exe.1 2>NUL
 type/?     2>type_cmd.2 >NUL
@@ -21,6 +23,7 @@ GOTO ERRORCHECK
 :AFTERTEST2
 
 
+REM -- TEST: HELP SWITCH W/ OTHER ARGS --
 type /T /? /p     >type_cmd.1 2>NUL
 type.exe /T /? /p >type_exe.1 2>NUL
 type /T /? /p     2>type_cmd.2 >NUL
@@ -28,6 +31,46 @@ type.exe /T /? /p 2>type_exe.2 >NUL
 
 GOTO ERRORCHECK
 :AFTERTEST3
+
+
+REM -- TEST: BOGUS SWITCHES --
+type /T /p     >type_cmd.1 2>NUL
+type.exe /T /p >type_exe.1 2>NUL
+type /T /p     2>type_cmd.2 >NUL
+type.exe /T /p 2>type_exe.2 >NUL
+
+GOTO ERRORCHECK
+:AFTERTEST4
+
+
+REM -- TEST: BAD FILENAME --
+type badfilename     >type_cmd.1 2>NUL
+type.exe badfilename >type_exe.1 2>NUL
+type badfilename     2>type_cmd.2 >NUL
+type.exe badfilename 2>type_exe.2 >NUL
+
+GOTO ERRORCHECK
+:AFTERTEST5
+
+
+REM -- TEST: BAD FILENAME W/ MULTPILE FILES --
+type badfile1 badfile2     >type_cmd.1 2>NUL
+type.exe badfile1 badfile2 >type_exe.1 2>NUL
+type badfile1 badfile2     2>type_cmd.2 >NUL
+type.exe badfile1 badfile2 2>type_exe.2 >NUL
+
+GOTO ERRORCHECK
+:AFTERTEST6
+
+
+REM -- TEST: MULTIPLE FILES --
+type .gitattributes .gitignore     >type_cmd.1 2>NUL
+type.exe .gitattributes .gitignore >type_exe.1 2>NUL
+type .gitattributes .gitignore     2>type_cmd.2 >NUL
+type.exe .gitattributes .gitignore 2>type_exe.2 >NUL
+
+GOTO ERRORCHECK
+:AFTERTEST7
 
 
 GOTO EXIT
--- type.c
+++ type.c
@@ -1,73 +1,88 @@
-#include <stdio.h>
 #include <windows.h>
 
-int main(int cmdc, char *cmda[]) {
+#define OUTPUT(out, wbuf, cbuf) \
+        ( WriteConsoleW((out), (wbuf), wcslen((wbuf)), NULL, NULL) \
+        || WideCharToMultiByte(CP_OEMCP, 0, (wbuf), -1, (cbuf), sizeof (cbuf), NULL, NULL) \
+        && WriteFile((out), (cbuf), strlen((cbuf)), NULL, NULL) ); \
+        FlushFileBuffers((out))
 
-    int i;
+int main() {
+
+    INT     i, argcw;
+    CHAR    cbuffer[8192];
+    WCHAR   wbuffer[8192];
+    DWORD   dwByteCount;
     HANDLE  hFile;
-    DWORD   dwBytesRead;
-    wchar_t buffer[2048];
-    char    cBuffer[2048];
     HANDLE  hStdOut = GetStdHandle(STD_OUTPUT_HANDLE);
-    int     argcw;
+    HANDLE  hStdErr = GetStdHandle(STD_ERROR_HANDLE);
     LPWSTR  *argvw = CommandLineToArgvW(GetCommandLineW(), &argcw);
 
     /* No arguments */
     if (argcw < 2) {
-        fputs("The syntax of the command is incorrect.\n", stderr);
+        OUTPUT(hStdErr, L"The syntax of the command is incorrect.\r\n", cbuffer);
         return 1;
     }
 
     /* /? switch */
-    for (i = 1; i < cmdc; i++) {
-        if (cmda[i][0] == '/' && cmda[i][1] == '?') {
-            puts("Displays the contents of a text file or files.\n\n"
-                 "TYPE [drive:][path]filename");
+    for (i = 1; i < argcw; i++) {
+        if (argvw[i][0] == '/' && argvw[i][1] == '?') {
+            OUTPUT(hStdOut, L"Displays the contents of a text file or files.\r\n\r\n"
+                            L"TYPE [drive:][path]filename\r\n", cbuffer);
             return 1;
         }
     }
 
     /* Bogus switches */
-    for (i = 1; i < cmdc; i++) {
-        if (cmda[i][0] == '/') {
-            fputs("The syntax of the command is incorrect.\n", stderr);
+    for (i = 1; i < argcw; i++) {
+        if (argvw[i][0] == '/') {
+            OUTPUT(hStdErr, L"The syntax of the command is incorrect.\r\n", cbuffer);
             return 1;
         }
     }
 
     /* Process files */
-    for (i = 1; i < cmdc; i++) {
+    for (i = 1; i < argcw; i++) {
 
         /* Attempt to open (wide-character name) file */
-        if ((hFile = CreateFileW(argvw[i], GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL)) == INVALID_HANDLE_VALUE) {
-            fputs("The system cannot find the file specified.\n", stderr);
+        if ((hFile = CreateFileW(argvw[i],
+                                 GENERIC_READ,
+                                 FILE_SHARE_READ,
+                                 NULL,
+                                 OPEN_EXISTING,
+                                 FILE_ATTRIBUTE_NORMAL,
+                                 NULL)
+                ) == INVALID_HANDLE_VALUE) {
+            OUTPUT(hStdErr, L"The system cannot find the file specified.\r\n", cbuffer);
             /* Specify file when multiple passed in */
-            if (cmdc > 2) fprintf(stderr, "Error occurred while processing: %s.\n", cmda[i]);
-            fflush(stdout);
+            if (argcw > 2) {
+                OUTPUT(hStdErr, L"Error occurred while processing: ", cbuffer);
+                OUTPUT(hStdErr, argvw[i], cbuffer);
+                OUTPUT(hStdErr, L".\r\n", cbuffer);
+            }
             continue;
         }
 
         /* Display filename, and blank lines, when there's multiple files */
-        if (cmdc > 2) fprintf(stderr, "\n%s\n\n\n", cmda[i]);
-        fflush(stderr);
+        if (argcw > 2) {
+            OUTPUT(hStdErr, L"\r\n", cbuffer);
+            OUTPUT(hStdErr, argvw[i], cbuffer);
+            OUTPUT(hStdErr, L"\r\n\r\n\r\n", cbuffer);
+        }
 
         /* Check byte-order mark for UTF-16 [little-endian] encoding */
-        ReadFile(hFile, buffer, sizeof (wchar_t), &dwBytesRead, NULL);
+        ReadFile(hFile, wbuffer, sizeof (wchar_t), &dwByteCount, NULL);
         /* UTF16le encoding ? */
-        if (buffer[0] == 0xFEFF) {
-            while ((ReadFile(hFile, buffer, sizeof buffer/2, &dwBytesRead, NULL), dwBytesRead)) {
-                WriteConsoleW(hStdOut, buffer, wcslen(buffer), NULL, NULL)
-                || WideCharToMultiByte(CP_OEMCP, 0, buffer, sizeof buffer/2, cBuffer, sizeof cBuffer, NULL, NULL)
-                && WriteFile(hStdOut, cBuffer, dwBytesRead/2, NULL, NULL)
-                && FlushFileBuffers(hStdOut);
+        if (wbuffer[0] == 0xFEFF) {
+            while ((ReadFile(hFile, wbuffer, sizeof wbuffer/2, &dwByteCount, NULL), dwByteCount)) {
+                OUTPUT(hStdOut, wbuffer, cbuffer);
             }
 
         /* ASCII encoding ? */
         } else {
             /* Rewind file pointer */
             SetFilePointer(hFile, 0, NULL, FILE_BEGIN);
-            while((ReadFile(hFile, cBuffer, sizeof cBuffer, &dwBytesRead, NULL)), dwBytesRead) {
-                WriteFile(hStdOut, cBuffer, dwBytesRead, NULL, NULL);
+            while((ReadFile(hFile, cbuffer, sizeof cbuffer, &dwByteCount, NULL)), dwByteCount) {
+                WriteFile(hStdOut, cbuffer, dwByteCount, NULL, NULL);
                 FlushFileBuffers(hStdOut);
             }
         }
